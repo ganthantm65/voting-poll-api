cc731000524944b8419eb448df47c8eb8f0feaf4
Ganthan.T.M <tmganthan@gmail.com>
2025-07-20 18:09:16 +0530

    1st-commit

---
+++ server/src/main/java/com/voting/VotingPollApp/VotingPollAppApplication.java
@@ -0,0 +1,13 @@
+package com.voting.VotingPollApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingPollAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotingPollAppApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/voting/VotingPollApp/VotingPollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voting.VotingPollApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingPollAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
